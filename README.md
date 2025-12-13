29caebb684ad93abe15f9dea0614be9636596d90
Beatriz Gonzales <blgonzalescastro@gmail.com>
2025-12-12 23:35:51 -0500

    dia3

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # data-science-g6
 REPOSITORIO BOOTCAPM DATA SCIENCE GRUPO G6
+
+## TEMARIO
+### MODULO 1
+Clase 1 Data Science
+Clase 2 Git y Github
+Clase 3 Python
---
+++ dia3/4-buclefor.py
@@ -0,0 +1,8 @@
+# BUCLE FOR
+for contador in range(1,10,1):
+    print(contador)
+#TABLA DE MULTIPLICAR
+tabla = int(input("Ingrese la tabla de multiplicar que desea ver:"))
+for contador in range(1,13,1):
+    resultado = tabla * contador
+    print(f"{tabla} x {contador} = {resultado}")
